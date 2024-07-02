f30f34d411bd55b1f1243cad148606bf41f58f93
pascalniri <pascalniri@gmail.com>
2024-07-02 16:33:43 +0200

    I have added a page for a single experience' explanations

---
+++ src/app/exp1/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Exp1 from '../../components/exp1/Exp1'
+
+
+const exp1 = () => {
+  return (
+    <div>
+        <Exp1 />
+    </div>
+  )
+}
+
+export default exp1
